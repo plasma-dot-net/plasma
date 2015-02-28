760d09ecb15d6e6328c30b2c0ff4bdcc19fb278d
David Whitney <david@davidwhitney.co.uk>
2015-02-28 23:25:15 +0000

    Removed manual versioning in favour of CI versioning.

--- src/Plasma.Core/Properties/AssemblyInfo.cs
+++ src/Plasma.Core/Properties/AssemblyInfo.cs
@@ -5,6 +5,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Plasma provides web automation and testing capabilities to ASP.NET projects.")]
 [assembly: AssemblyCompany("n/a")]
 [assembly: AssemblyProduct("Plasma")]
-[assembly: AssemblyCopyright("Copyright 2011")]
+[assembly: AssemblyCopyright("Copyright 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
